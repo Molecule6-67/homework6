d58b2893211119d95f48c3154e12c5b9f65e8bc3
Aydar Sadykov <6darch6@vk.com>
2024-07-27 19:24:05 +0500

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,17 @@
+# dict = {"Ivan": 89784643726, "Vadim": 89376278473, "Kiram": 89353457326}
+# print(dict)
+# print(dict["Ivan"])
+# dict["Nina"] = 89865534332
+# print(dict)
+# dict.update({'Bil': 8909236876,
+#             'Volya': 89230523746,
+#             'Sonya': 89436532623})
+# print(dict)
+# del dict['Ivan']
+# print(dict)
+set_ = {1, 2, 3, 5, 4568, 689, "DARVIN", "GIF", 1, 2, 3, 4, 5, 6, 7, "darvin"}
+print(set_)
+set_.add("Friends")
+set_.add(66969)
+set_.discard("darvin")
+print(set_)
